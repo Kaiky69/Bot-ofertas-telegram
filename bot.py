661a3c8c2144dd876d430b2292fa024b3e6a4200
Kaiky69 <150951818+Kaiky69@users.noreply.github.com>
2026-03-16 01:14:01 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -4,37 +4,17 @@ import random
 TOKEN = "8540004525:AAGQTirxzKJLYssGqZl9kqxTAs23XY9mh18"
 CHAT_ID = "-1003551593842"
 
-buscas = [
-"fone bluetooth",
-"smartwatch",
-"ssd",
-"placa de video",
-"notebook"
+produtos = [
+"🔥 Oferta do dia\nhttps://meli.la/exemplo1",
+"🔥 Super desconto\nhttps://meli.la/exemplo2",
+"🔥 Promoção imperdível\nhttps://meli.la/exemplo3"
 ]
 
-produto = random.choice(buscas)
+mensagem = random.choice(produtos)
 
-url = f"https://api.mercadolibre.com/sites/MLB/search?q={produto}"
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-dados = requests.get(url).json()
-
-item = random.choice(dados["results"])
-
-titulo = item["title"]
-preco = item["price"]
-link = item["permalink"]
-
-mensagem = f"""
-🔥 OFERTA ENCONTRADA
-
-📦 {titulo}
-💰 R${preco}
-
-🛒 Comprar:
-{link}
-"""
-
-requests.post(
-f"https://api.telegram.org/bot{8540004525:AAGQTirxzKJLYssGqZl9kqxTAs23XY9mh18}/sendMessage",
-data={"-1003551593842": -1003551593842, "text": mensagem}
-)
+requests.post(url, data={
+ "chat_id": CHAT_ID,
+ "text": mensagem
+})
