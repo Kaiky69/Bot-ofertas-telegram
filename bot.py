493c35ae5234b5366c08bc2d4a313defee192c29
Kaiky69 <150951818+Kaiky69@users.noreply.github.com>
2026-03-16 00:41:16 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,40 @@
+import requests
+import random
+
+TOKEN = "SEU_TOKEN"
+CHAT_ID = "SEU_CANAL_ID"
+
+buscas = [
+"fone bluetooth",
+"smartwatch",
+"ssd",
+"placa de video",
+"notebook"
+]
+
+produto = random.choice(buscas)
+
+url = f"https://api.mercadolibre.com/sites/MLB/search?q={produto}"
+
+dados = requests.get(url).json()
+
+item = random.choice(dados["results"])
+
+titulo = item["title"]
+preco = item["price"]
+link = item["permalink"]
+
+mensagem = f"""
+🔥 OFERTA ENCONTRADA
+
+📦 {titulo}
+💰 R${preco}
+
+🛒 Comprar:
+{link}
+"""
+
+requests.post(
+f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+data={"chat_id": CHAT_ID, "text": mensagem}
+)
